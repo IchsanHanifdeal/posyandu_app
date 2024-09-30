982ba332cfceaff98ad8bce15b2f13f0beb7f26e
zaadevofc <zaadevfc@gmail.com>
2024-09-30 19:01:47 +0700

    [UP] fix error gajelas

--- resources/views/dashboard/pelayanan_dokter.blade.php
+++ resources/views/dashboard/pelayanan_dokter.blade.php
@@ -18,58 +18,55 @@
           </div>
         @endforeach
         <div class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-5">
-          @foreach (['Evaluasi Kesehatan Ibu Hamil', 'Skrining Preeklampsia', 'Pemeriksaan Dokter Trimester 1', 'Pemeriksaan Dokter Trimester 3'] as $item)
-            <label for="add_data_{{ str_replace(' ', '_', $item) }}" class="flex flex-row items-center gap-3 cursor-pointer card rounded-xl py-3 px-6 border border-primary">
+          @foreach (['Evaluasi Kesehatan Ibu Hamil', 'Skrining Preeklampsia', 'Pemeriksaan Dokter Trimester 1', 'Pemeriksaan Dokter Trimester 3'] as $i => $item)
+            <label onclick="add_data_{{ str_replace(' ', '_', $item) }}.showModal();initInput('{{ $item }}', '{{ $i }}')"
+              class="flex flex-row items-center gap-3 cursor-pointer card rounded-xl py-3 px-6 border border-primary">
               <x-lucide-scroll-text class="size-6 stroke-[1]" />
               <h1 class="font-semibold">{{ $item }}</h1>
             </label>
           @endforeach
         </div>
       </div>
-
-      <div class="w-full hidden" id="container_surat">
-        <div class="flex items-center ml-auto gap-3 drop-shadow-md border sticky top-[5.4rem] bg-[#FCE7F3] z-10 p-4 rounded-xl mb-3">
-          <h1 class="btn btn-sm" onclick="cetak('preview_surat_amanat_persalinan')">Tutup</h1>
-          <h1 class="btn btn-sm btn-primary ml-auto" onclick="print()">Cetak Surat</h1>
-        </div>
-        <div id="preview_surat_amanat_persalinan" class="[&_section]:!h-fit !w-max !max-w-1/2 border  rounded-xl !p-0 [&>div]:!p-0 [&>div]:overflow-hidden [&>div]:rounded-lg">
-        </div>
-      </div>
     </div>
   @else
   @endif
-  @foreach (['Evaluasi Kesehatan Ibu Hamil', 'Skrining Preeklampsia', 'Pemeriksaan Dokter Trimester 1', 'Pemeriksaan Dokter Trimester 3'] as $item)
-    <dialog id="add_data_{{ str_replace(' ', '_', $item) }}" class="modal modal-bottom sm:modal-middle">
-      <form onsubmit="saveData(this)" action="javascript:void();"S class="modal-box">
-        <h3 class="text-lg font-bold">Lengkapi Dokumen</h3>
-        <div class="flex flex-col py-4" id="input_render_{{ str_replace('_', ' ', $item) }}"></div>
-        <div class="modal-action">
-          <button type="submit" class="btn btn-primary">
-            Simpan
-          </button>
-          <button class="btn" type="button" onclick="document.getElementById(`add_data_{{ $item }}`).close();">
-            Tutup
-          </button>
-        </div>
-      </form>
-    </dialog>
-  @endforeach
 </x-dashboard.main>
 
+@foreach (['Evaluasi Kesehatan Ibu Hamil', 'Skrining Preeklampsia', 'Pemeriksaan Dokter Trimester 1', 'Pemeriksaan Dokter Trimester 3'] as $item)
+  <dialog id="add_data_{{ str_replace(' ', '_', $item) }}" class="modal modal-bottom sm:modal-middle">
+    <form onsubmit="saveData(this)" action="javascript:void();"S class="modal-box">
+      <h3 class="text-lg font-bold">Lengkapi Dokumen</h3>
+      <div class="flex flex-col py-4" id="input_render_{{ str_replace('_', ' ', $item) }}"></div>
+      <div class="modal-action">
+        <button type="submit" class="btn btn-primary">
+          Simpan
+        </button>
+        <button class="btn" type="button" onclick="document.getElementById(`add_data_{{ str_replace(' ', '_', $item) }}`).close();">
+          Tutup
+        </button>
+      </div>
+    </form>
+  </dialog>
+@endforeach
 
 <script>
-  let file = '/docx/bkiabi-amanat-persalinan.docx';
-  document.addEventListener('DOMContentLoaded', async () => {
-    const data = await extractDocx({
-      file
+  let init_file = '';
+  let files = [
+    '/docx/bkiabi-evaluasi-kesehatan-ibu.docx',
+    '/docx/bkiabi-skrining-preeklampsia.docx',
+    '/docx/bkiabi-pemeriksaan-dokter-trimester-1.docx',
+    '/docx/bkiabi-pemeriksaan-dokter-trimester-3.docx',
+  ]
+
+  async function initInput(item, i) {
+    init_file = files[i];
+    let data = await extractDocx({
+      file: files[i]
     })
 
-    const keys = await extractDocxKeys(data)
-    const filter = keys.original().filter(x => !x.startsWith('%'));
+    let keys = await extractDocxKeys(data)
+    let filter = keys.original().filter(x => !x.startsWith('%'));
 
-    @foreach (['Evaluasi Kesehatan Ibu Hamil', 'Skrining Preeklampsia', 'Pemeriksaan Dokter Trimester 1', 'Pemeriksaan Dokter Trimester 3'] as $item)
-      renderFormInputs(filter, "input_render_{{ $item }}")
-    @endforeach
-
-  });
+    renderFormInputs(filter, "input_render_" + item);
+  }
 </script>
