23594eb281334f0eae7e8afd2237cac32b21eb9d
zaadevofc <zaadevfc@gmail.com>
2024-10-05 19:41:33 +0700

    [UP] merge with main

--- resources/views/dashboard/identitas_anak.blade.php
+++ resources/views/dashboard/identitas_anak.blade.php
@@ -19,82 +19,6 @@
             </p>
           </div>
         </div>
-<<<<<<< HEAD
-      @endforeach
-    </div>
-    <div class="flex gap-5">
-      @foreach (['Identitas_anak_terdaftar'] as $item)
-        <div class="flex flex-col w-full border-back rounded-xl">
-          <div class="p-5 bg-white sm:p-7 rounded-t-xl">
-            <h1 class="flex items-start gap-3 font-semibold font-[onest] text-lg capitalize">
-              {{ str_replace('_', ' ', $item) }}
-            </h1>
-            <p class="text-sm opacity-60">
-              Ketahui Identitas Anak Anda.
-            </p>
-          </div>
-          <div class="flex flex-col gap-3 p-5 pt-0 divide-y rounded-b-xl sm:p-7">
-            <div class="overflow-x-auto">
-              <table class="table w-full table-zebra">
-                <thead>
-                  <tr>
-                    @foreach (['No', 'No Surat', 'Nama Anak', 'Nama Ibu', 'Hari', 'Tanggal', 'Pukul', 'Jenis Kelamin', 'Jenis Kelahiran', 'Kelahiran Ke', 'Berat Badan', 'Panjang Badan', 'Tempat Kelahiran'] as $header)
-                      <th class="font-bold text-center uppercase">{{ $header }}</th>
-                    @endforeach
-                  </tr>
-                </thead>
-                <tbody class="capitalize">
-                  @forelse ($anak as $i => $item)
-                    <tr>
-                      <th class="text-center">{{ $i + 1 }}</th>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->no_surat ?? '-' }}</td>
-                      <td class="font-semibold text-center capitalize">{{ $item->nama ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->ibu->user->nama ?? '-' }}</td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ \Carbon\Carbon::parse($item->tanggal)->translatedFormat('l') ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ \Carbon\Carbon::parse($item->tanggal)->translatedFormat('d F Y') ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">{{ $item->pukul ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->jenis_kelamin ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->jenis_kelahiran ?? '-' }}</td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->kelahiran_ke ?? '-' }}
-                      </td>
-                      <td class="font-semibold text-center capitalize">{{ $item->berat ?? '-' }}
-                        Kg
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->panjang ?? '-' }} Cm
-                      </td>
-                      <td class="font-semibold text-center capitalize">
-                        {{ $item->tempat_kelahiran ?? '-' }}</td>
-                    </tr>
-                  @empty
-                    <tr>
-                      <td colspan="14" class="text-gray-500 text-center">Tidak Ada Anak
-                        Terdaftar
-                      </td>
-                    </tr>
-                  @endforelse
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      @endforeach
-    </div>
-  @elseif(Auth::user()->role === 'user')
-  @endif
-=======
         <div class="flex flex-col lg:flex-row gap-5">
             @foreach (['tambah'] as $item)
                 <div onclick="{{ $item . '_modal' }}.showModal()"
@@ -324,5 +248,4 @@
         @endforeach
     @elseif(Auth::user()->role === 'user')
     @endif
->>>>>>> origin
 </x-dashboard.main>
